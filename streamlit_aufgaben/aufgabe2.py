32ba117e6c0eda8e15e2f2383b8398130c30f2f5
Margan Ghafoori <marjan@MacBook-Pro-von-Marjan.local>
2026-05-31 00:28:55 +0200

    Aufgabe2 doctrings sichtbar/kommentar verwendet

--- streamlit_aufgaben/aufgabe2.py
+++ streamlit_aufgaben/aufgabe2.py
@@ -2,21 +2,20 @@ import streamlit as st
 
 st.title("Session State: Zähler-App")
 
-"""
-Falscher Ansatz ohne st.session_state:
 
-counter = 0
 
-if st.button("Hochzählen"):
-    counter +=1
+#Falscher Ansatz ohne st.session_state:
 
-st.write(counter)
+#counter = 0
 
-Problem:
-Bei jedem Klick wird das gesamte Skript neu ausgeführt.
-Dadurch wird counter immer wieder auf 0 gesetzt und zählt nicht korrekt weiter.
+#if st.button("Hochzählen"):
+#    counter +=1
 
-"""
+#st.write(counter)
+
+#Problem:
+#Bei jedem Klick wird das gesamte Skript neu ausgeführt.
+#Dadurch wird counter immer wieder auf 0 gesetzt und zählt nicht korrekt weiter.
 
 if "counter" not in st.session_state:
     st.session_state.counter = 0
